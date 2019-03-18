000256a891aa87d912e5523ff0e67939b7fecde8
Mario <mariosergio23.03.01@hotmail.com>
2019-03-18 11:44:48 -0300

    Novo Avatar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-
-import "./App.css";
-import Header from "./components/Header";
-import NewUser from "./components/NewUser";
+import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cor: "white",
+      contador: 0
+    };
+  }
+
+  trocarCor = e => {
+    let contador = this.state.contador;
+    let cor = ++contador % 2 == 0 ? "red" : "blue";
+    this.setState({
+      cor: cor,
+      contador: contador
+    });
+  };
+
   render() {
+    const estilo = {
+      padding: 10,
+      background: this.state.cor
+    };
     return (
       <div>
-        <Header />
-        <NewUser />
+        <h1 style={estilo} onClick={this.trocarCor}>
+          {this.props.texto}
+        </h1>
+        <h2> Contador {this.state.contador} </h2>
       </div>
     );
   }
